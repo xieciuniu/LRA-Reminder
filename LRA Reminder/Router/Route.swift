a63212559b05ba056a68b80903f8b5e65b120a2f
Hubert Wojtowicz <hubertw@backbase.com>
2025-10-09 14:52:21 +0200

    add route enum

---
+++ LRA Reminder/Router/Route.swift
@@ -0,0 +1,14 @@
+//
+//  Route.swift
+//  LRA Reminder
+//
+//  Created by Hubert Wojtowicz on 09/10/2025.
+//
+
+import Foundation
+
+enum Route: Hashable {
+    case home
+    case reminderList
+    case reminder
+}
